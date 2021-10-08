5a65bf7084432400af5faea3df80cf0a9ea9a7fa
kouki-02 <k-doi2270@st.kyoto-art.ac.jp>
2021-10-08 14:42:16 +0900

    changed_js
    Update script01.js
    
    Update script01.js

---
+++ sample/js/script01.js
@@ -0,0 +1,12 @@
+$(function(){
+    
+    $("#button01").on("click",function(){
+        $(".back-red").hide();
+    })
+})
+
+$("#button02").on("click",function(){
+    $(".back-blue").hide();
+    })
+
+    
