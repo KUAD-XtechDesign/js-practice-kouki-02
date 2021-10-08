8f22699262fbb09aee18782e980dfabf293baf9d
kouki-02 <k-doi2270@st.kyoto-art.ac.jp>
2021-10-08 15:56:26 +0900

    changed_js

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,6 +1,6 @@
 $(function() {
-    let txt1 = "おはようございます";
-    let txt2 = "おげんきですか";
+    let txt1 = "おはようございます!";
+    let txt2 = "おげんきですか?";
     let num1 = 20;
     let num2 = 32;
     $("#button01").on("click", function() {
